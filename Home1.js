b4b0f58b0cd5163811b554d60f42042ff0c8b5ab
M G <1871mg@gmail.com>
2016-12-08 16:26:00 +0300

    Add files via upload

---
+++ Home1.js
@@ -0,0 +1,22 @@
+'use strict';
+
+//Д/з на 08.12.2016
+console.log('Задача 1');
+var a = 2, b = 4, c = 6;
+var s = 2 * a * b + 2 * a * c + 2 * b * c; 
+console.log('Площадь прямоугольного параллелепипеда =', s, 'единиц площади.');
+
+console.log('                ');
+
+console.log('Задача 2');
+var s = 1000;
+var sgryadka = 15*25;
+var sfree = s%sgryadka;
+console.log('Свободная площадь =', sfree, 'кв. м.');
+
+console.log('                ');
+
+console.log('Задача 3');
+var s1 = 15;
+var s2 = 6;
+console.log('Площадь кольца =', s1 - s2, 'кв. дм.');
